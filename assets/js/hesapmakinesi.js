546875ef9183142e20abaeb3f6bd96d2863d9e88
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2025-01-09 01:08:47 +0300

    css düzeltildi

--- assets/js/hesapmakinesi.js
+++ assets/js/hesapmakinesi.js
@@ -1,24 +1,37 @@
-// screen, process, process-end sıfırlama, esittir
 let result = document.querySelector('.screen h1');
 let btns = document.querySelectorAll('.process button');
 let sonucBtn = document.querySelector('.process-end .esittir');
 let silme = document.querySelector('button.btn.btn-del');
 let sifirlama = document.querySelector('.process-end .sifirlama');
 
+let isResultDisplayed = false;
+
 function handleBtnClick() {
-  result.innerText += this.innerText 
+  const lastChar = result.innerText.slice(-1);
+
+  if (['+', '-', '*', '/'].includes(lastChar) && ['+', '-', '*', '/'].includes(this.innerText)) {
+    return; 
+  }
+
+  if (isResultDisplayed) {
+    result.innerText = ""; 
+    isResultDisplayed = false; 
+  }
+  
+  result.innerText += this.innerText;
 }
 
-for(let i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', handleBtnClick);
 }
 
 function handleSonucClick() {
-  result.innerText = eval(result.innerText);
+    result.innerText = eval(result.innerText); 
+    isResultDisplayed = true;
 }
 
 function handleDelete() {
-  result.innerText = ' ';
+  result.innerText = "";
 }
 
 sifirlama.addEventListener('click', handleDelete);
